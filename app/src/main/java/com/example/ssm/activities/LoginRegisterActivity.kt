6f3e60b58f265b86c80f8909182f00c6aab704f8
M1RAGE1110 <jovitocolin@gmail.com>
2022-12-19 22:05:34 +0700

    start

---
+++ app/src/main/java/com/example/ssm/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ssm.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ssm.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
